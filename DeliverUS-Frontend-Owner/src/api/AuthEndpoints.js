3944596438d515279df174bee78e197d8b89c4d0
dlozaco975@gmail.com <dlozaco975@gmail.com>
2025-03-26 10:03:57 +0100

    syntax fixed

--- DeliverUS-Frontend-Owner/src/api/AuthEndpoints.js
+++ DeliverUS-Frontend-Owner/src/api/AuthEndpoints.js
@@ -1,18 +1,18 @@
 import { post, put } from './helpers/ApiRequestsHelper'
 
-function login(data) {
+function login (data) {
   return post('users/loginOwner', data)
 }
 
-function register(data) {
+function register (data) {
   return post('users/registerOwner', data)
 }
 
-function update(data) {
+function update (data) {
   return put('users', data)
 }
 
-function isTokenValid(storedToken) {
+function isTokenValid (storedToken) {
   return put('users/isTokenValid', { token: storedToken })
 }
 
